aa145cb8145ec5d991c87aa10319e5393bb7bba3
Shuichi Machida <shuichi.machida@gmail.com>
2019-12-18 20:46:24 +0900

    test

---
+++ src/main/java/jp/vmware/sol/demo/App.java
@@ -0,0 +1,13 @@
+package jp.vmware.sol.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
